4b72ad7a7c1780b10238bd56e56485109f5987b5
Aleksei <ancientt1k1@gmail.com>
2024-01-31 11:35:21 +0300

    Добавлено отображение результата

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -7,19 +7,132 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CalculationError: Error {
+    case dividedByZero
+}
 
+enum Operation: String {
+    case add = "+"
+    case substract = "-"
+    case multiply = "x"
+    case divide = "/"
+    
+    func calculate(_ number1: Double, _ number2: Double) throws -> Double {
+        switch self {
+            case .add:
+                return number1 + number2
+            case .substract:
+                return number1 - number2
+            case .multiply:
+                return number1 * number2
+            case .divide:
+                if number2 == 0 {
+                    throw CalculationError.dividedByZero
+                }
+                return number1 / number2
+        }
+    }
+}
+
+enum CalculationHistoryItem {
+    case number(Double)
+    case operation(Operation)
+}
+
+final class ViewController: UIViewController {
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
         guard let buttonText = sender.currentTitle else { return }
         
-        print(buttonText)
+        if buttonText == "," && label.text?.contains(",") == true {
+            return
+        }
+        
+        if (label.text == "0" || label.text == "Ошибка") && buttonText != "," {
+            label.text = buttonText
+        } else if label.text == "Ошибка" && buttonText == "," {
+            label.text = "0,"
+        } else {
+            label.text?.append(buttonText)
+        }
     }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        guard let buttonText = sender.currentTitle,
+              let buttonOperation = Operation(rawValue: buttonText),
+              let labelText = label.text,
+              let labelNumber = numberForamatter.number(from: labelText)?.doubleValue else {
+            return
+        }
+        
+        calculationHistory.append(.number(labelNumber))
+        calculationHistory.append(.operation(buttonOperation))
+        
+        resetLabelText()
+    }
+    
+    @IBAction func clearButtonPressed() {
+        calculationHistory.removeAll()
+        resetLabelText()
+    }
+    
+    @IBAction func calculateButtonPressed() {
+        guard let labelText = label.text,
+              let labelNumber = numberForamatter.number(from: labelText)?.doubleValue else {
+            return
+        }
+        
+        calculationHistory.append(.number(labelNumber))
+        
+        do {
+            let result = try calculate()
+            label.text = numberForamatter.string(from: NSNumber(value: result))
+        } catch {
+            label.text = "Ошибка"
+        }
+        
+        calculationHistory.removeAll()
+    }
+    
+    @IBOutlet weak var label: UILabel!
+    
+    lazy var numberForamatter: NumberFormatter = {
+        let numberForamatter = NumberFormatter()
+        
+        numberForamatter.usesGroupingSeparator = false
+        numberForamatter.locale = Locale(identifier: "ru_Ru")
+        numberForamatter.numberStyle = .decimal
+        
+        return numberForamatter
+    }()
+    
+    var calculationHistory: [CalculationHistoryItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resetLabelText()
     }
-
-
+    
+    func calculate() throws -> Double {
+        guard case .number(let firstNumber) = calculationHistory[0] else { return 0 }
+        
+        var currentResult = firstNumber
+        
+        for index in stride(from: 1, to: calculationHistory.count - 1, by: 2) {
+            guard case .operation(let operation) = calculationHistory[index],
+                  case .number(let number) = calculationHistory[index+1] else {
+                break
+            }
+            
+            currentResult = try operation.calculate(currentResult, number)
+        }
+        
+        return currentResult
+    }
+    
+    func resetLabelText() {
+        label.text = "0"
+    }
 }
 
